852206b7633b9716912fb8789bc8f05a70ee1697
Oran Can Ören <orancanoren@gmail.com>
2018-06-12 21:43:24 +0300

    - fixed diffie hellman

--- cryptoran/main.py
+++ cryptoran/main.py
@@ -4,15 +4,15 @@ from packages import BlockCiphers, PublicKeyCrypto, SecretKeySharing
 if __name__ == '__main__':
     def banner(description):
         print('''
-                        _                        
-                        | |                       
-    ___ _ __ _   _ _ __ | |_ ___  _ __ __ _ _ __  
+                          _                        
+                         | |                       
+     ___ _ __ _   _ _ __ | |_ ___  _ __ __ _ _ __  
     / __| '__| | | | '_ \\| __/ _ \\| '__/ _` | '_ \\ 
-    | (__| |  | |_| | |_) | || (_) | | | (_| | | | |
+   | (__| |  | |_| | |_) | || (_) | | | (_| | | | |
     \\___|_|   \\__, | .__/ \\__\\___/|_|  \\__,_|_| |_|
-                __/ | |                             
+             __/ | |                             
             |___/|_|                             ''')
-        print('-----------------', description, '-----------------')
+        print('    ----------------------', description.upper(), '----------------------')
 
     blockciphers = ['aes', 'des']
     modes = ['cbc', 'ecb']
@@ -37,6 +37,9 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input', help='input in ASCII format')
     parser.add_argument('-e', '--encrypt', help='encrypt input', action='store_true')
     parser.add_argument('-d', '--decrypt', help='decryption input', action='store_true')
+    parser.add_argument('-p', '--prime', help='group order for key exchange and pkc protocols', type=int)
+    parser.add_argument('-pl', '--primelength', help='bitlength for prime number', type=int)
+    parser.add_argument('-g', '--generator',help='group generator for key exchange and pkc protocols', type=int)
 
     args = parser.parse_args()
 
@@ -51,6 +54,8 @@ if __name__ == '__main__':
             parser.error('--mop is required for block ciphers')
         if not args.encrypt and not args.decrypt:
             parser.error('block ciphers should be invoked in encryption or decryption mode')
+        if args.decrypt and not args.key:
+            parser.error('key must be provided to block ciphers in decryption mode')
         if not args.input:
             parser.error('an input must be provided to the cryptosystem')
         if args.mode != 'ecb' and not args.initvector and args.decrypt:
@@ -69,3 +74,12 @@ if __name__ == '__main__':
         else:
             block = [int(args.input, 16)]
             print(cipher.decrypt(block))
+        
+    elif args.cryptosystem in secretkey:
+        if not args.prime and not args.primelength:
+            parser.error('A prime group order or prime length must be supplied with key exchange protocols')
+        exchange = SecretKeySharing.DiffieHellman(args.prime, args.generator, args.primelength)
+        prime, generator, expSecret = exchange.generateSecret()
+        print(f'Multiplicative group properties:\nPrime (largest group element + 1):\n{prime}\nGenerator:\n{generator}\nThis party sends:\n{expSecret}')
+        correspondentExp = int(input('\nPlease enter the correspondent input:\n>> '))
+        print('Shared secret:\n', exchange.generateSharedKey(correspondentExp))
