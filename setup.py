8d6f56a0212c2d6b7cfc31b85c0277377a70531f
Oran Can Ören <orancanoren@gmail.com>
2018-06-12 15:02:10 +0300

    - added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+long_description = 'Cryptoran is a Python3 based cryptographic algorithms library. It implements block ciphers, public key crypto algorithms and key exchange protocols. It is not safe right now and I would NOT suggest using it for sensitive material. I am developing the library for educational purposes only. Feel free to contribute and tinker around with the library.\n\nHappy encrypting!\n\n-orancanoren'
+
+setup(name='cryptoran',
+      description='A Python3 based cryptographic algorithms library',
+      long_description=long_description,
+      version='0.0.1',
+      url='https://github.com/orancanoren/cryptoran',
+      author='Oran Can Oren',
+      author_email='orancanoren@gmail.com',
+      license='MIT',
+      packages=['cryptosuite']
+)
